03bfbaa34e62bb18155f115e1215136099267f80
unknown <gauravjoshi171998@gmail.com>
2025-11-06 16:09:42 +0530

    shift mongodb online to server

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
+require('dotenv').config()
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json()) // req.body
+
+const PORT = process.env.PORT || 3000
 
 // Import the routes files
 const personRoutes = require('./routes/personRoutes')
@@ -12,6 +15,6 @@ const menuRoutes = require('./routes/menuRoutes')
 app.use('/person', personRoutes)
 app.use('/menuItem', menuRoutes)
 
-app.listen(5000, () => {
-  console.log(`Server listening on port ${5000}`)
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
 })
